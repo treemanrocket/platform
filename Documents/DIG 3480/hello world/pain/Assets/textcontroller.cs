2e43f03c58dc3af119c2b3eebf06c4b6bff8bfa9
Albert Cheung <albert52798@knights.ucf.edu>
2023-01-30 15:35:43 -0800

    January 30, 2023 3:35 PM

---
+++ Documents/DIG 3480/hello world/pain/Assets/textcontroller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class textcontroller : MonoBehaviour
+{
+    public Text newText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown ("space"))
+        {
+            newText.text = "Hello, my name is Albert Cheung";
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
